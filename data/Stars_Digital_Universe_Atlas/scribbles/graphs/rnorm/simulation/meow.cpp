c83bb914ce33b340f11e4b34440c03be0f58ed0d
Aswini S <ashumeow@live.com>
2015-05-01 03:00:56 +0530

    modify it to meow

--- data/Stars_Digital_Universe_Atlas/scribbles/graphs/rnorm/simulation/meow.cpp
+++ data/Stars_Digital_Universe_Atlas/scribbles/graphs/rnorm/simulation/meow.cpp
@@ -1,10 +1,9 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 // [[Rcpp::export]]
-double piSugar(const int N) {
+double meow(const int N) {
   RNGScope scope;		// ensure RNG gets set/reset
   NumericVector x = runif(N);
-  NumericVector y = runif(N);
-  NumericVector d = sqrt(x*x + y*y);
+  NumericVector d = sqrt(x*x);
   return 4.0 * sum(d < 1.0) / N;
 }
